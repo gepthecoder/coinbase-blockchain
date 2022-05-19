620c24385ba603f8a28bc1775498f85e2c094730
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-19 22:17:58 +0200

    $0021: [Dashboard] [Sidebar]: create sidebar component + add coinbase logo

--- coinbase-blockchain-app/pages/Dashboard.js
+++ coinbase-blockchain-app/pages/Dashboard.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import styled from "styled-components"
 import Main from '../components/Main'
+import Sidebar from "../components/Sidebar"
 
 const Dashboard = ({ address }) => {
 
   return (
       <Wrapper>
+          <Sidebar />
           <MainContainer>
             <Header />
             <Main />
