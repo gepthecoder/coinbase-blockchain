2c885e80fd3c64c27698de755898a31fc578abd7
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-20 12:21:20 +0200

    $0035: [Dashboard] [TotalBalance] [Portfolio]: api request for getting temp sanity tokens

--- coinbase-blockchain-app/components/Portfolio.js
+++ coinbase-blockchain-app/components/Portfolio.js
@@ -3,8 +3,25 @@ import { BsThreeDotsVertical } from 'react-icons/bs'
 import { coins } from '../static/coins'
 import Coin from './Coin'
 import BalanceChart from './BalanceChart'
+import { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+
+    const [sanityTokens, setSanityTokens] = useState([])
+
+    useEffect(() => {
+        const getCoins = async () => {
+          try {
+              const coins = await fetch("https://r0608fwk.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22coins%22%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D")
+              const tempSanityTokens = await coins.json()
+              console.log(tempSanityTokens)
+              setSanityTokens(tempSanityTokens.result)
+          } catch (error) {
+              console.log(error)
+          }
+        }
+        getCoins()
+    }, [])
   
   return (
     <Wrapper>
