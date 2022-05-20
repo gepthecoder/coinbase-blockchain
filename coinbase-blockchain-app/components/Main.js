45661c2af8c8b515a983ef566db08d7cade144c6
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-20 09:28:45 +0200

    $0026: [Dashboard] [Promos]: add two promo offer cards and place them in main.js

--- coinbase-blockchain-app/components/Main.js
+++ coinbase-blockchain-app/components/Main.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import Portfolio from './Portfolio'
+import Promos from './Promos'
 
 const Main = () => {
   return (
     <Wrapper>
       <Portfolio />
+      <Promos />
     </Wrapper>
   )
 }
--- coinbase-blockchain-app/components/Promos.js
+++ coinbase-blockchain-app/components/Promos.js
@@ -1,8 +1,28 @@
 import styled from 'styled-components'
 
 const Promos = () => {
-    return <div></div>
+    return (
+        <Wrapper>
+            <OfferCard>
+                <Title>Yield earned</Title>
+                <Description>Earn up TO 7.77% APY on your crypto</Description>
+                <Placeholder />
+                <Additional style={{ fontSize: '1.5rem' }}>
+                    $0.000066 <span>2.84% APY</span>                    
+                </Additional>
+            </OfferCard>
+
+            <OfferCard>
+                <Title>Learn and Earn</Title>
+                <Description>Earn up CO 2.84% APY on your crypto</Description>
+                <Placeholder />
+                <Additional style={{ color: '#3773f5' }}>Verify Identity</Additional>
+            </OfferCard>
+        </Wrapper>
+    )
 }
+
+export default Promos
 
 
 const Wrapper = styled.div`
