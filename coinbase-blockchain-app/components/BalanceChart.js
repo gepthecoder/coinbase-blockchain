a29892cbe4685fa5dfd02d351e1437dd2eb00265
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-20 10:27:19 +0200

    $0030: [Dashboard] [BalanceChart]: prepare simple data structure and use it to render line on graph

--- coinbase-blockchain-app/components/BalanceChart.js
+++ coinbase-blockchain-app/components/BalanceChart.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import { Line } from 'react-chartjs-2'
+
+const data = {
+    labels: [],
+    datasets: [],
+}
 
 const BalanceChart = () => {
     return (
       <Wrapper>
+            <Line data={data} options={options} width={400} height={150} />
       </Wrapper>
     )
   }
