a8f38e051772fe7588f0e22416cbb18904f4fb09
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-20 10:52:02 +0200

    $0031: [Dashboard] [BalanceChart]: show balance chart in portfolio.js

--- coinbase-blockchain-app/components/Portfolio.js
+++ coinbase-blockchain-app/components/Portfolio.js
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { coins } from '../static/coins'
 import Coin from './Coin'
+import BalanceChart from './BalanceChart'
 
 const Portfolio = () => {
   
   return (
     <Wrapper>
       <Content>
+        <BalanceChart />
         <PortfolioTable>
             <TableItem>
                 <Title>Your Assets</Title>
