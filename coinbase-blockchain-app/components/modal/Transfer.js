bbf2f98be6d7ac17dd79c6162e8d77541915b3d3
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-21 10:52:26 +0200

    $0049: [ModalRouting] [TransferModal] [Transfer]: add style for wrapper

--- coinbase-blockchain-app/components/modal/Transfer.js
+++ coinbase-blockchain-app/components/modal/Transfer.js
@@ -1,10 +1,20 @@
+import styled from 'styled-components'
 
 
 const Transfer = ({ setAction, twTokens, selectedToken, walletAddress }) => {
 
   return (
-    <h2>Thats kinda cool</h2>
+      <Wrapper>
+        <h2>Thats kinda cool</h2>
+      </Wrapper>
   )
 }
 
 export default Transfer
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex: 1;
+`
